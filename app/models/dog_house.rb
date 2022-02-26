6c7ad22bb0dbc50f8d194c2acdc1048f6740e2a3
Ashley Mattam <amattam427@gmail.com>
2022-02-25 23:49:38 -0500

    commit tree b5909ae69be92aff31cb2e7d5262e819e17d694f

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,6 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  #has_many :reviews
+
+  #updated to below so that we don't have to do any extra steps to delete a Doghouse - this will allow us to run DogHouse.first.destroy to delete the doghouse, w/out having to delete the associated model first. 
+  has_many :reviews, dependent: :destroy
 end
